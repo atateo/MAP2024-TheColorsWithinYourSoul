5589c07465626c1157d518b216f986c631ccb82d
yuri.tateo.03 <yuri.tateo@gmail.com>
2024-06-19 18:32:22 +0200

    Creazione della classe RoomConnection e prima stesura della classe Room

---
+++ src/main/java/it/uniba/map/giocotestuale/entities/RoomConnection.java
@@ -0,0 +1,33 @@
+package it.uniba.map.giocotestuale.entities;
+
+public class RoomConnection {
+
+    private Room reachableRoom;
+
+    private boolean isLocked;
+
+    public RoomConnection(final Room reachableRoom, final boolean isLocked) {
+        this.reachableRoom = reachableRoom;
+        this.isLocked = isLocked;
+    }
+
+    public Room getReachableRoom() {
+        return reachableRoom;
+    }
+
+    public void setReachableRoom(final Room reachableRoom) {
+        this.reachableRoom = reachableRoom;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void setLocked(final boolean isLocked) {
+        this.isLocked = isLocked;
+    }
+
+    public void unlock() {
+        this.isLocked = false;
+    }
+}
