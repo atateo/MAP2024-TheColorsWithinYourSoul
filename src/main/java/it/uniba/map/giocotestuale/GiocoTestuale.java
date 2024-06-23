b6306a956492aca10329703043d0519dcf230b6f
yuri.tateo.03 <yuri.tateo@gmail.com>
2024-06-23 20:53:01 +0200

    Rimossi commenti non necessari in GiocoTestuale.java

--- src/main/java/it/uniba/map/giocotestuale/GiocoTestuale.java
+++ src/main/java/it/uniba/map/giocotestuale/GiocoTestuale.java
@@ -30,70 +30,7 @@ public class GiocoTestuale {
 			logger.info("Costruisco il database");
 			Setup.costruisciDatabase();
 		}
-		
-		MenuGUI menu = new MenuGUI();
-		
-//		//test CRUD
-//		Contenuto contenuto = new Contenuto();
-//		contenuto.setLabel("PROVA");
-//		contenuto.setMessaggio("messaggio di prova");
-//		contenuto.setRisposta(false);
-//		contenuto.setIdItem(12);
-//		
-//		ContenutoDaoImpl contImpl = new ContenutoDaoImpl();
-//		try {
-//			contImpl.add(contenuto);
-//			Contenuto contenuto1 = contImpl.getContenuto(1);
-//			System.out.println(contenuto1!=null?"contenuto1: "+contenuto1.toString():"Contenuto è nullo");
-//			
-//			List<Contenuto> lista = contImpl.getContenuti();
-//			for (Contenuto contenuto2 : lista) {
-//				System.out.println("contenuto2: "+contenuto2.toString());
-//			}
-//			Contenuto contUp = contImpl.getContenuto(1);
-//			if(contUp!=null) {
-//				contUp.setMessaggio("prova di aggiornamento tramite DAO");
-//				contImpl.update(contUp);
-//				
-//				contImpl.delete(1);
-//			}
-//			
-//			
-//			
-//			
-//		}
-//		catch (SQLException e) {
-//			e.printStackTrace();
-//			// TODO: handle exception
-//		}
 
-//		Item itemTest = new Item(0, "Test1", null, "0");
-//		Room room = new Room(0, "Test1", "0");
-//
-//		System.out.println(itemTest.getStatus());
-//		System.out.println(room.getStatus());
-//
-//		SingleObjectInteraction testSingleInteraction = new SingleObjectInteraction(
-//				itemTest, Command.USA, "0", "1",
-//				(targetObject, resultObject, targetState, resultState) -> {
-//					if(targetObject.getStatus().equals(targetState))
-//						targetObject.setStatus(resultState);
-//		});
-//
-//		TwoObjectInteraction testTwoInteraction = new TwoObjectInteraction(
-//			itemTest, room, Command.USA, "1", "1",
-//			(targetObject, resultObject, targetState, resultState) -> {
-//				if(targetObject.getStatus().equals(targetState))
-//					targetObject.setStatus(resultState);
-//			}
-//		);
-//
-//		testSingleInteraction.executeInteraction();
-//		testTwoInteraction.executeInteraction();
-//
-//		System.out.println(itemTest.getStatus());
-//		System.out.println(room.getStatus());
-		
 		logger.debug("Connessione rilasciata");
 		DatabaseConnection.releaseConnection();
 	}
