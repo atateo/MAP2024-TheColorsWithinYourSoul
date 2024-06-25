e955f4ac27746bb47551fee0e13253834ce3f010
Antimo Tateo <a.tateo@studenti.uniba.it>
2024-06-25 07:41:28 +0200

    update client rest
    update client rest

--- src/main/java/it/uniba/map/giocotestuale/utility/ClientRest.java
+++ src/main/java/it/uniba/map/giocotestuale/utility/ClientRest.java
@@ -10,6 +10,10 @@ import java.net.URL;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import it.uniba.map.giocotestuale.config.ApplicationProperties;
 
 public class ClientRest {
@@ -30,7 +34,17 @@ public class ClientRest {
 		//String secret="020b12d3f0ee4c4e00653ea153635b93";
 		String secret=appProps.getSecret();
 		// TODO Auto-generated method stub
-		executePost(url+"?client_id="+clientID+"&client_secret="+secret, "");
+		String jsonToken = executePost(url+"?client_id="+clientID+"&client_secret="+secret, "");
+		
+		JsonObject jsonObject = JsonParser.parseString(jsonToken).getAsJsonObject();
+		String token = jsonObject.get("token").getAsString();
+		String url2="https://api.artsy.net/api/artists/vincent-van-gogh";
+		String vangogh= executePost(url2,token);
+		
+		JsonObject jsonObject2 = JsonParser.parseString(vangogh)
+			    .getAsJsonObject();
+		JsonArray artworks = (JsonArray) jsonObject2.get("artworks");
+		
 
 	}
 	public static String executePost(String targetURL, String urlParameters) {
@@ -45,6 +59,9 @@ public class ClientRest {
 
 			connection.setRequestMethod("POST");
 			connection.setRequestProperty("User-Agent", USER_AGENT);
+			//connection.setRequestProperty("Authorization", "X-XAPP-Token eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI1YjdiNjY4OS03MjMwLTQwMzEtOWNmYi1kOTdkZTJiZDk5ZDkiLCJleHAiOjE3MTk3NzM1NjAsImlhdCI6MTcxOTE2ODc2MCwiYXVkIjoiNWI3YjY2ODktNzIzMC00MDMxLTljZmItZDk3ZGUyYmQ5OWQ5IiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjY2Nzg2ZWY4ZTAyYjZkMDAwZjZmNDAzNyJ9.yKapRhASqce9Ddc12MmbvwdDJCdt1FQveizcgRFwNjY");
+			if(urlParameters!=null && !urlParameters.equals(""))
+				connection.setRequestProperty("X-XAPP-Token", urlParameters);
 			
 			connection.setDoOutput(true);
 			OutputStream os = connection.getOutputStream();
