bf76826d486f10e42fe34d37fdeb87d70ec09158
AngeloVincenti <162358075+AngeloVincenti@users.noreply.github.com>
2024-06-30 23:35:36 +0200

    Aggiunta dei commenti javadoc in GameGUI

--- src/main/java/it/uniba/map/giocotestuale/gui/GameGUI.java
+++ src/main/java/it/uniba/map/giocotestuale/gui/GameGUI.java
@@ -22,42 +22,46 @@ import javax.swing.JLabel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
-
+// Classe che mostra la GUI del gioco 
 
 public class GameGUI extends JPanel {
 
-    private JButton back;
+    private JButton back;  // pulsante per tornare indietro al menu principale
 
-    private JButton save;
+    private JButton save; // pulsante per salvare il gioco
 
-    private JButton help;
+    private JButton help; // pulsante di aiuto, mostra la finestra dei comandi di gioco
 
-    private static JButton audio;
+    private static JButton audio; // pulsante per attivare o disattivare l'audio del gioco
 
-    private static JLabel timerLabel;
+    private static JLabel timerLabel; // label per posizionare il timer
 
-    private JLabel timerImgLabel1;
+    private JLabel timerImgLabel1; // label per l'icona sinistra del timer
 
-    private JLabel timerImgLabel2;
+    private JLabel timerImgLabel2; // label per l'icona destra del timer
 
-    private JLabel inventoryLabel;
+    private JLabel inventoryLabel; // label per l'inventario 
 
-    private static JPanel imagePanel;
+    private static JPanel imagePanel; // pannello che mostra l'immagine di gioco corrente
 
-    private static JTextPane displayTextPane;
+    private static JTextPane displayTextPane; // TextPane che mostra il testo del gioco
 
-    private JScrollPane scrollPaneDisplayText;
+    private JScrollPane scrollPaneDisplayText; // ScrollPane per il testo del gioco
 
-    private static JTextArea inventoryTextArea;
+    private static JTextArea inventoryTextArea; // TextArea che mostra il testo dell'inventario
 
-    private JScrollPane scrollPaneInventoryText;
+    private JScrollPane scrollPaneInventoryText; // ScrollPAne per l'inventario 
 
-    private JTextField userInputField;
+    private JTextField userInputField; // TextField che riceve in input i comandi dell'utente
 
-    private JToolBar toolBar;
+    private JToolBar toolBar; // ToolBar che comprende il timer e i vari pulsanti
 
-    private static CardLayout cardLayout;
+    private static CardLayout cardLayout; // CardLayout che consente di cambiare GUI
 
+    /**
+    * Costruttore pubblico che imposta la scrollbar a 0 e chiama i metodi initComponents e initCurrentImage.
+    * per settare i componenti a video e l'immagine di gioco corrente.
+    */
     public GameGUI()
     {
         UIManager.put("ScrollBar.width", 0);
@@ -66,22 +70,28 @@ public class GameGUI extends JPanel {
         initCurrentImage();
     }
 
+    /**
+    * Metodo che si occupa di configurare e posizionare tutte le componenti sullo schermo.
+    */
     private void initComponents()
     {
+        // configurazione del pannello
         this.setVisible(true);
         this.setSize(1000,700);
 
-
+        // configurazione del pannello per l'immagine
         imagePanel = new JPanel();
         imagePanel.setSize(400,400);
         imagePanel.setBorder(BorderFactory.createLineBorder(new Color(168, 129, 50), 5));
 
+        // configurazione della toolBar
         toolBar = new JToolBar();
         toolBar.setFloatable(false);
         toolBar.setBorderPainted(false);
         toolBar.setBackground(new Color(50, 168, 156));
         toolBar.add(Box.createHorizontalStrut(10));
 
+        // configurazione del pulsante back 
         back = new JButton(new ImageIcon("src/main/resources/img/backButton.png"));
         back.setFocusPainted(false);
         back.setBackground(new Color(166, 15, 15));
@@ -90,8 +100,10 @@ public class GameGUI extends JPanel {
         back.addActionListener(this::BackActionPerformed);
         toolBar.add(back);
 
+        // aggiunta del gap per il posizionamento
         toolBar.add(Box.createHorizontalStrut(20));
 
+        // configurazione del pulsante save
         save = new JButton(new ImageIcon("src/main/resources/img/saveButton.png"));
         save.setFocusPainted(false);
         save.setBackground(new Color(166, 15, 15));
@@ -99,8 +111,10 @@ public class GameGUI extends JPanel {
         save.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
         toolBar.add(save);
 
+        // aggiunta del gap per il posizionamento
         toolBar.add(Box.createHorizontalStrut(20));
 
+        // configurazione del pulsante help
         help = new JButton(new ImageIcon("src/main/resources/img/help_icon.png"));
         help.setFocusPainted(false);
         help.setBackground(new Color(166, 15, 15));
@@ -109,8 +123,10 @@ public class GameGUI extends JPanel {
         help.addActionListener(this::HelpActionPerformed);
         toolBar.add(help);
 
-        toolBar.add(Box.createHorizontalStrut(20));
-
+        // aggiunta del gap per il posizionamento
+        toolBar.add(Box.createHorizontalStrut(20);
+        
+        // configurazione del pulsante audio
         audio = new JButton(new ImageIcon("src/main/resources/img/audio_icon_game.png"));
         audio.setFocusPainted(false);
         audio.setBackground(new Color(166, 15, 15));
@@ -118,8 +134,10 @@ public class GameGUI extends JPanel {
         audio.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
         toolBar.add(audio);
 
+        // aggiunta del gap per il posizionamento
         toolBar.add(Box.createHorizontalStrut(100));
 
+        // configurazione della timerLabel
         timerLabel = new JLabel();
         timerLabel.setHorizontalTextPosition(SwingConstants.CENTER);
         timerLabel.setVerticalTextPosition(SwingConstants.CENTER);
@@ -128,6 +146,7 @@ public class GameGUI extends JPanel {
         timerLabel.setOpaque(true);
         timerLabel.setBackground(new Color(168, 129, 50));
 
+        // configurazione delle icone per il timer
         timerImgLabel1 = new JLabel(new ImageIcon("src/main/resources/img/timerImg.png"));
         timerImgLabel2 = new JLabel(new ImageIcon("src/main/resources/img/timerImg.png"));
         toolBar.add(timerImgLabel1);
@@ -136,6 +155,7 @@ public class GameGUI extends JPanel {
         toolBar.add(Box.createHorizontalStrut(20));
         toolBar.add(timerImgLabel2);
 
+        // aggiunta dello sfondo dell'inventario
         ImageIcon inventoryImg = new ImageIcon("src/main/resources/img/sfondoInventario.jpg");
 
         JViewport inventoryView = new JViewport() {
@@ -147,12 +167,12 @@ public class GameGUI extends JPanel {
         };
 
 
-
+        // aggiunta e posizionamento dell'icona borsaInventario 
         inventoryLabel = new JLabel(new ImageIcon("src/main/resources/img/borsaInventario.png"));
         inventoryLabel.setSize(40,40);
         inventoryLabel.setBounds((GroupLayout.PREFERRED_SIZE)+530,(GroupLayout.PREFERRED_SIZE)+10,40,40);
 
-
+        // configurazione dell'inventoryTextArea
         inventoryTextArea = new JTextArea();
         inventoryTextArea.setEditable(false);
         inventoryTextArea.setAutoscrolls(false);
@@ -164,16 +184,14 @@ public class GameGUI extends JPanel {
         inventoryTextArea.setText("Inventario:");
         inventoryTextArea.setForeground(Color.BLACK);
         inventoryTextArea.add(inventoryLabel);
-
-
-
-
+        
+        // configurazione dello scrollPaneInventoryText
         scrollPaneInventoryText = new JScrollPane();
         scrollPaneInventoryText.setSize(440,400);
         scrollPaneInventoryText.setViewport(inventoryView);
         scrollPaneInventoryText.setViewportView(inventoryTextArea);
 
-
+        // aggiunta dello sfondo per il testo del gioco
         ImageIcon img = new ImageIcon("src/main/resources/img/sfondoTela.jpg");
         JViewport view = new JViewport() {
             @Override
@@ -183,6 +201,7 @@ public class GameGUI extends JPanel {
             }
         };
 
+        // configurazione di displayTextPane
         displayTextPane = new JTextPane();
         displayTextPane.setEditable(false);
         displayTextPane.setFocusable(false);
@@ -191,6 +210,7 @@ public class GameGUI extends JPanel {
         displayTextPane.setOpaque(false);
         displayTextPane.setForeground(new Color(0, 0, 0));
 
+        // configurazione dello scrollPaneDisplayText
         scrollPaneDisplayText = new JScrollPane();
         scrollPaneDisplayText.setViewport(view);
         scrollPaneDisplayText.setViewportView(displayTextPane);
@@ -199,6 +219,7 @@ public class GameGUI extends JPanel {
         scrollPaneDisplayText.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPaneDisplayText.setBorder(BorderFactory.createLineBorder(new Color(168, 129, 50), 5));
 
+        // configurazione dell'userInputField
         userInputField = new JTextField();
         userInputField.setOpaque(false);
         userInputField.setSize(400,107);
@@ -206,7 +227,7 @@ public class GameGUI extends JPanel {
         userInputField.setBorder(BorderFactory.createLineBorder(new Color(168, 129, 50), 5));
         userInputField.addActionListener(this::UserInputActionPerformed);
 
-
+       // aggiunta dello sfondo per l'user input
         JPanel userInputFieldPanel = new JPanel()
         {
             @Override
@@ -215,13 +236,14 @@ public class GameGUI extends JPanel {
                 g.drawImage(img.getImage(), 0, 0, getWidth(), getHeight(), this);
             }
         };
+        // configurazione dell'userInputFieldPanel
         userInputFieldPanel.setLayout(null);
         userInputFieldPanel.setSize(400,107);
         userInputFieldPanel.setBorder(BorderFactory.createLineBorder(new Color(168,129,50),7));
         userInputFieldPanel.add(userInputField);
 
 
-        // Setting the layout of the panel
+        // impostazione del Group Layout per il posizionamento dei vari componenti sul pannello
         GroupLayout layout = new GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -256,6 +278,9 @@ public class GameGUI extends JPanel {
 
     }
 
+    /**
+    * Metodo che si occupa di impostare l'immagine di gioco corrente
+    */
     public void initCurrentImage() {
 
         cardLayout = new CardLayout();
@@ -278,16 +303,29 @@ public class GameGUI extends JPanel {
         imagePanel.setLayout(cardLayout);
     }
 
+    /**
+    * Metodo che definisce il comportamento del pulsante back quando viene cliccato.
+    * Passa al menu principale selezionando MenuGUI attraverso il CardLayout.
+    * @param evt rappresenta l'evento del click sul pulsante.
+     */
     private void BackActionPerformed(ActionEvent evt) {
         CardLayout cl = (CardLayout) getParent().getLayout();
         cl.show(getParent(), "MenuGUI");
     }
 
+    /**
+    * Metodo che definisce il comportamento del pulsante help quando viene cliccato.
+    * Apre la finestra comandi chiamando il metodo getIstance di CommandsGUI.
+    * @param evt rappresenta l'evento del click sul pulsante.
+    */
     private void HelpActionPerformed(ActionEvent evt) {
         CommandsGUI help = CommandsGUI.getIstance();
         help.setVisible(true);
     }
-
+    /**
+    * Metodo che gestisce l'input dell'utente.
+    * @param evt rappresenta l'evento dell'input dell'utente.
+    */
     private void UserInputActionPerformed(ActionEvent evt) {
         String text = userInputField.getText();
         userInputField.setText("");
